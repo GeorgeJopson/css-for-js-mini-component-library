8eee79cf189f24300e3bfe607f8c6ca1a9ce6b6f
George Jopson <georgejopson@gmail.com>
2026-07-23 16:10:00 +0100

    Add initial attempt at Select.js

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,49 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <SelectBox>
+      {displayedValue}
+      <PositionedIcon id="chevron-down"/>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+    </SelectBox>
   );
 };
+
+const PositionedIcon = styled(Icon)`
+    position: absolute;
+    right: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+`
+
+const NativeSelect = styled('select')`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  appearance: none;
+`;
+
+const SelectBox = styled(`button`)`
+    height: 43px;
+    background-color: ${COLORS.transparentGray15};
+    padding: 12px 52px 12px 16px;
+    position: relative;
+    width: fit-content;
+    font-size: 16px;
+    font-weight: 400;
+    color: ${COLORS.gray700};
+    border-radius: 8px;
+    
+    border: none;
+    
+    &:hover{
+        color: black;
+    }
+`
 
 export default Select;
