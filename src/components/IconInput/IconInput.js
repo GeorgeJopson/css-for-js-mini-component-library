703728db18be8b67f384e5564f7a3a2db509f6c3
George Jopson <georgejopson@gmail.com>
2026-07-23 18:51:00 +0100

    Get basics of IconInput done

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -13,7 +13,36 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  return <Wrapper width={width}>
+
+    <InputIcon id={icon}/><InputBar  size={size}></InputBar>
+  </Wrapper>;
 };
+
+const Wrapper = styled('div')`
+    width:${({ width }) => width}px;
+    position: relative;
+`
+const InputIcon = styled(Icon)`
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+    width: 24px;
+    height: 24px;
+    pointer-events: none;
+`
+
+const InputBar = styled('input')`
+    display: inline-block;
+    border: none;
+    width: 100%;
+    border-bottom: solid ${({ size }) => size==="large"?2:1}px;
+    font-size: calc(18rem /16);
+    padding-top: 8px;
+    padding-bottom: 8px;
+    padding-left: 36px;
+`
 
 export default IconInput;
