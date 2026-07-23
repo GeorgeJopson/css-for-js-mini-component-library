96d97d163786db4b3c590ff1c84da597c2b5e50b
George Jopson <georgejopson@gmail.com>
2026-07-23 20:55:25 +0100

    Implement small and large sizes for IconInput.js

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,23 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  "large":{
+    padding: "8px",
+    fontSize: `${18/16}rem`,
+    iconWidth: 2,
+    iconSize: 24,
+    paddingLeft: "36px"
+  },
+  "small":{
+    padding: "4px",
+    fontSize: `${14/16}rem`,
+    iconWidth: 1,
+    iconSize: 16,
+    paddingLeft: "24px"
+  }
+}
+
 const IconInput = ({
   label,
   icon,
@@ -16,7 +33,7 @@ const IconInput = ({
   return <Wrapper width={width}>
     <VisuallyHidden>{label}</VisuallyHidden>
     <InputBar  size={size} placeholder={placeholder}></InputBar>
-    <InputIcon id={icon} strokeWidth={2}/>
+    <InputIcon id={icon} strokeWidth={SIZES[size].iconWidth} size={SIZES[size].iconSize} />
   </Wrapper>;
 };
 
@@ -31,9 +48,11 @@ const InputBar = styled('input')`
     width: 100%;
     border-bottom: solid ${({ size }) => size==="large"?2:1}px;
     font-size: calc(18rem /16);
-    padding-top: 8px;
-    padding-bottom: 8px;
-    padding-left: 36px;
+    
+    padding-top: ${({ size }) => SIZES[size].padding};
+    padding-bottom: ${({ size }) => SIZES[size].padding};
+    
+    padding-left: ${({ size }) => SIZES[size].paddingLeft};
     font-weight: 700;
     color: ${COLORS.gray700};
     &::placeholder {
@@ -56,8 +75,8 @@ const InputIcon = styled(Icon)`
     top: 0;
     bottom: 0;
     margin: auto;
-    width: 24px;
-    height: 24px;
+    width: ${({size})=>size}px;
+    height: ${({size})=>size}px;
     pointer-events: none;
     color: ${COLORS.gray700};
     ${InputBar}:hover + & {
